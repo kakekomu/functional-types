5d0bd6b70c38b2397fbed1be3fd388d04b1bc0e9
Gergely Szabo <gege251@gmail.com>
2019-07-18 06:51:20 +0900

    fix builds

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { AsyncRemoteData, Failure, Loading, NotAsked, RemoteData, RemoteDataList, Success } from "./remote-data";
-import * as remoteData from "./remote-data";
-import * as restClient from "./rest-client";
-export { AsyncRemoteData, RemoteData, RemoteDataList, Success, NotAsked, Loading, Failure, remoteData, restClient };
